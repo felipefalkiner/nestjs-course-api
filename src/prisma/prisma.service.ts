a2a54f4c7f38b622986e5c68a915d3441bcaaf79
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2023-02-16 20:08:18 -0300

    added @nestjs/config and updated typo on database config

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,7 +7,7 @@ export class PrismaService extends PrismaClient {
     super({
       datasources: {
         db: {
-          url: 'postgresql://porgres:123456@localhost:5434/nest?schema=public',
+          url: 'postgresql://postgres:123456@localhost:5434/nest?schema=public',
         },
       },
     });
